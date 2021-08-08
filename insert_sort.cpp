70abf320fe7fef9c27bc7e0d07d0f01ac272ceab
shensunbo <53904062+shensunbo@users.noreply.github.com>
2021-08-08 17:28:43 +0800

    Add files via upload

---
+++ insert_sort.cpp
@@ -0,0 +1,22 @@
+﻿#include"insert_sort.h"
+
+void insert_sort(datatype* a, int N)
+{
+	int tmp;
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = i; j > 0; j--)
+		{
+			if (a[j] > a[j - 1])
+				break;
+			if (a[j] < a[j - 1])
+			{
+				tmp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = tmp;
+
+			}
+			
+		}
+	}
+}
---
+++ insert_sort.h
@@ -0,0 +1,6 @@
+#ifndef _INSERT_SORT
+#define _INSERT_SORT
+typedef int datatype;
+void insert_sort(datatype *a,int N);
+#endif // !_INSERT_SORT
+
